a456743488bb751de94502e5025ab38f74ac09c1
xiongchen <xiongchen@myai.tech>
2021-08-27 16:17:49 +0800

    改一下avatar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const Layout = ({ location, title, children }) => {
     const rootPath = `${__PATH_PREFIX__}/`;
@@ -8,21 +9,31 @@ const Layout = ({ location, title, children }) => {
 
     if (isRootPath) {
         header = (
-            <h2 className="main-heading">
-                <Link to="/">{title}</Link>
-            </h2>
+            <div style={{ display: 'inline-flex' }}>
+                <StaticImage
+                    className='w-16 h-16 rounded-full mx-auto'
+                    src='../images/profile-pic.jpg'
+                    alt=''
+                />
+                <h3
+                    className='main-heading'
+                    style={{ display: 'inline-block', padding: 8 }}
+                >
+                    <Link to='/'>{title}</Link>
+                </h3>
+            </div>
         );
     } else {
         header = (
-            <Link className="header-link-home" to="/">
+            <Link className='header-link-home' to='/'>
                 {title}
             </Link>
         );
     }
 
     return (
-        <div className="global-wrapper" data-is-root-path={isRootPath}>
-            <header className="global-header">{header}</header>
+        <div className='global-wrapper' data-is-root-path={isRootPath}>
+            <header className='global-header'>{header}</header>
             <main>{children}</main>
         </div>
     );
